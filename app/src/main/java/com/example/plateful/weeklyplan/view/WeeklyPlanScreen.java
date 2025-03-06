76331adb25523dd6dc7459bcd306299061a8ebe3
Amira Mohamed <amiramohameda745@gmail.com>
2025-03-06 17:28:30 +0200

    Added delete icon to card

--- app/src/main/java/com/example/plateful/weeklyplan/view/WeeklyPlanScreen.java
+++ app/src/main/java/com/example/plateful/weeklyplan/view/WeeklyPlanScreen.java
@@ -5,15 +5,29 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.plateful.R;
+import com.google.android.material.chip.ChipGroup;
 
 public class WeeklyPlanScreen extends Fragment {
 
+    private ImageView imageViewProfile;
+
+    private TextView textViewDateChosen;
+
+    private ChipGroup chipGroupDays;
+
+    private RecyclerView recyclerViewWeeklyMeals;
+
+    private WeeklyPlanAdapter weeklyPlanAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,5 +44,17 @@ public class WeeklyPlanScreen extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        imageViewProfile = view.findViewById(R.id.imageView_ic_profile_photo);
+
+        textViewDateChosen = view.findViewById(R.id.textView_DateChosen);
+
+        chipGroupDays = view.findViewById(R.id.chipGroupDays);
+
+        recyclerViewWeeklyMeals = view.findViewById(R.id.recyclerView_Weekly_Meals);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(requireContext(), 2);
+        recyclerViewWeeklyMeals.setLayoutManager(gridLayoutManager);
+
+
     }
 }
